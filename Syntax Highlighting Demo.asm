d10feda73008ce2f3f666b6099268e2bbd93d513
Load A <143745153+load-a@users.noreply.github.com>
2023-09-11 15:19:55 -0400

    Forgot the demo

---
+++ Syntax Highlighting Demo.asm
@@ -0,0 +1,32 @@
+//Hack Assembly Syntax Highlighting Demo
+
+//Normal Comment
+//*Doc Comment
+
+(Label)
+@Label
+@variable
+@1234567890
+@SP
+@R15
+@ARG
+@SCREEN
+AMD=1
+0;JMP
+
+(Initialize)
+  @256
+  D=A
+  @SP
+  M=D
+  @420
+  D=A
+  @blaze_it
+  M=D
+  @End
+  D;JEQ
+(End)
+  @End
+  0;JMP
+
+
